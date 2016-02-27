d0f63511984b8c0a9faeedf2ecb877b870a3667f
Zalmoxisus <zalmoxisus@gmail.com>
2016-02-27 14:22:06 +0200

    Don't process feed when there are no new posts

--- src/app/sagas/feed.js
+++ src/app/sagas/feed.js
@@ -7,7 +7,7 @@ import { fetchFeed } from '../actions/api';
 import { groupByTarget } from '../utils/feedUtils';
 import { updateTrayIcon } from '../services/electron';
 import notify from '../utils/notifications';
-import { getToken, getFeed } from './selectors';
+import { getToken, getPosts } from './selectors';
 
 const delay = ms => new Promise(resolve => setTimeout(resolve, ms));
 
@@ -16,19 +16,21 @@ export function* setReloadTimeout() {
   yield put(fetchFeed.request());
 }
 
-function* areNewPosts(feed, host) {
-  const current = yield select(getFeed, host);
-  return current && feed.result[0] !== current.getIn(['result', 0]);
-}
-
 export function* loadFeed({ host = 'github', participating }) {
   try {
     const token = yield select(getToken, host);
     let feed = yield call(api.fetchFeed, host, token, { participating });
     feed = normalize(feed, arrayOf(post), { assignEntity: assignEntity() });
-    updateTrayIcon(feed.result.length);
-    if (yield areNewPosts(feed, host)) notify(feed);
-    yield put(fetchFeed.success({ host, payload: groupByTarget(feed) }));
+    const result = feed.result;
+    const count = result.length;
+    updateTrayIcon(count);
+    const posts = yield select(getPosts, host);
+    const firstDiff = !posts || result[0] !== posts.first();
+    if (posts && firstDiff) notify(feed);
+
+    if (firstDiff || result[count - 1] !== posts.last()) {
+      yield put(fetchFeed.success({ host, payload: groupByTarget(feed) }));
+    } else yield put(fetchFeed.success());
   } catch (error) {
     console.error(error);
     yield put(fetchFeed.error(error));
--- src/app/sagas/selectors.js
+++ src/app/sagas/selectors.js
@@ -2,3 +2,4 @@ export const isAuthorized = (state, host) => state.auth.has(host);
 export const getToken = (state, host) => state.auth.getIn([host, 'token']);
 export const getUserName = (state, host) => state.auth.getIn([host, 'login']);
 export const getFeed = (state, host) => state.feed.get(host);
+export const getPosts = (state, host) => state.feed.getIn([host, 'result']);
