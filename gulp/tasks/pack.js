5664835dee67ce937e14ba261d8b7a4e63c120f9
Zalmoxisus <zalmoxisus@gmail.com>
2016-03-14 21:31:26 +0200

    Pack for linux and windows as well

--- gulp/tasks/pack.js
+++ gulp/tasks/pack.js
@@ -7,15 +7,28 @@ const packagerOptions = {
   dir: path.resolve(__dirname, '../../build/electron'),
   out: path.resolve(__dirname, '../../build'),
   name: config.productName,
-  platform: 'darwin',
   arch: 'all',
-  icon: path.resolve(__dirname, '../../src/electron/resources/osx/icon.icns'),
   version: config.electronVersion,
   'app-version': config.version
 };
 
-gulp.task('pack:electron', () => {
-  packager(packagerOptions, function (error, appPath) {
+function pack(options, cb) {
+  packager({ ...packagerOptions, ...options }, function (error, appPath) {
     if (error) throw error;
+    else if (cb) cb();
+  });
+}
+
+gulp.task('pack:electron', () => {
+  pack({ platform: 'linux' }, () => {
+    pack({
+      platform: 'darwin',
+      icon: path.resolve(__dirname, '../../src/electron/resources/osx/icon.icns')
+    }, () => {
+      pack({
+        platform: 'win32',
+        icon: path.resolve(__dirname, '../../src/electron/resources/windows/icon.ico')
+      });
+    });
   });
 });
